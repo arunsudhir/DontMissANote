c02d777d870bfdf82e8f0d09d0cf8fe79c5cbff9
Jorge <jogonzal@microsoft.com>
2015-05-07 12:45:26 -0700

    Fixing visual studio deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var routes = require('./routes/index');
 var callback = require('./routes/callback');
@@ -38,4 +39,9 @@ app.use(function (err, req, res, next) {
     });
 });
 
-module.exports = app;
+app.set('port', process.env.PORT || 3000);
+
+// Run
+http.createServer(app).listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
+});
