60a2b97b082e3dfa8aa3a558483408aa44de05e7
Jorge <jogonzal@microsoft.com>
2015-05-10 11:02:12 -0700

    Fix https to http

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     //Live Connect API information
     clientId: '000000004C151513',
     clientSecret: 'a9eXH-eBOtik7AHTsaEznsWEgkddAIwZ ',
-    redirectUrl: 'https://onenotealerts.herokuapp.com/callback'
+    redirectUrl: 'http://onenotealerts.herokuapp.com/callback'
 };
