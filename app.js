ffcf0dd0127e762089df986601857439c08be94f
Dipak Boyed <diboyed@microsoft.com>
2015-05-10 10:44:10 -0700

    use new domain, http port 80
    all you need to update your hosts file after syncing this checkin

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var http = require('http');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     //Live Connect API information
     clientId: '000000004C151513',
     clientSecret: 'a9eXH-eBOtik7AHTsaEznsWEgkddAIwZ ',
-    redirectUrl: 'http://onenoteapisamples.com:3000/callback'
+    redirectUrl: 'https://onenotealerts.herokuapp.com/callback'
 };
